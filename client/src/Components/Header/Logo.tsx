7c150e5b0eadd0eed9cf9616a59a820f32b3e2ab
XggXWolf <cetinmert058@gmail.com>
2026-03-14 19:34:46 +0300

    Add router link to logo

--- client/src/Components/Header/Logo.tsx
+++ client/src/Components/Header/Logo.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router";
+
 export default function Logo() {
     return (
-        <div className="logo-main text-2xl font-bold tracking-wider text-blue-400">
-            <span className="text-white">BATTLE</span>SHIPS
-        </div>
+        <Link to="/">
+            <div className="logo-main text-2xl font-bold tracking-wider text-blue-400">
+                <span className="text-white">BATTLE</span>SHIPS
+            </div>
+        </Link>
     );
 }
