9be74415c80dc868743d40a56ca3b622fc3136be
XggXWolf <cetinmert058@gmail.com>
2026-04-05 21:38:23 +0300

    start nestjs authentication, switch to pnpm (only for backend right now)

--- client/src/Components/Layout.tsx
+++ client/src/Components/Layout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router";
 import Header from "./Shared/Header/Header";
 import Background from "./Shared/Background/Background";
-import PlayerCount from "./Shared/PlayerCount/PlayerCount";
 
 interface LayoutProps {
     sonar?: boolean;
