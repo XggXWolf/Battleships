7400b925f8acb36e2114699c67de7d1a902183d4
XggXWolf <cetinmert058@gmail.com>
2026-05-08 22:00:20 +0300

    Ignore generated prisma folder

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -3,7 +3,7 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
-export default defineConfig({
+module.exports = defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
